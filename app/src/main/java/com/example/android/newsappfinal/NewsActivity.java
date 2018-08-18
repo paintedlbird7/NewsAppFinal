45fff71bb6a8f4c583883f9009bc6995e5af5e69
paintedlbird7 <rosaperezlarios@gmail.com>
2018-08-18 14:34:20 -0700

    adding settings preferences

--- app/src/main/java/com/example/android/newsappfinal/NewsActivity.java
+++ app/src/main/java/com/example/android/newsappfinal/NewsActivity.java
@@ -5,12 +5,16 @@ import android.app.LoaderManager.LoaderCallbacks;
 import android.content.Context;
 import android.content.Intent;
 import android.content.Loader;
+import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -28,9 +32,16 @@ public class NewsActivity extends AppCompatActivity
      * URL for news data from the Guardian
      */
     private static final String GUARDIAN_REQUEST_URL =
-            "https://content.guardianapis.com/search?from-date=2018-06-01" +
-                    "&to-date=2018-08-01&q=Android&api-key=7ee07fcd-fd06-4ee3-85a3-bdaa67850658&show-tags=" +
-                    "contributor&page-size=10";
+
+           // "https://earthquake.usgs.gov/fdsnws/event/1/query";
+
+      "https://content.guardianapis.com/search?from-date=2018-06-01" +
+              "&to-date=2018-08-01&q=Android&api-key=7ee07fcd-fd06-4ee3-85a3-bdaa67850658&show-tags=" +
+              "contributor&page-size=10";
+//
+//            "https://content.guardianapis.com/search?from-date=2018-06-01" +
+//                    "&to-date=2018-08-01&q=Android&api-key=7ee07fcd-fd06-4ee3-85a3-bdaa67850658&show-tags=" +
+//                    "contributor&page-size=10";
 
     /**
      * Constant value for the news loader ID. We can choose any integer.
@@ -110,12 +121,31 @@ public class NewsActivity extends AppCompatActivity
         }
     }
 
-
+    // Create a new loader for the given URL
     @Override
     public Loader<List<News>> onCreateLoader(int i, Bundle bundle) {
-        // Create a new loader for the given URL
-        Log.i(LOG_TAG, "TEST: onCreateLoader() is called ...");
-        return new NewsLoader(this, GUARDIAN_REQUEST_URL);
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+
+//        String minMagnitude = sharedPrefs.getString(
+//                getString(R.string.settings_min_magnitude_key),
+//                getString(R.string.settings_min_magnitude_default));
+
+        String orderBy = sharedPrefs.getString(
+                getString(R.string.settings_order_by_key),
+                getString(R.string.settings_order_by_default)
+        );
+
+        Uri baseUri = Uri.parse(GUARDIAN_REQUEST_URL);
+        Uri.Builder uriBuilder = baseUri.buildUpon();
+        uriBuilder.appendQueryParameter("format", "geojson");
+        uriBuilder.appendQueryParameter("limit", "10");
+        //uriBuilder.appendQueryParameter("minmag", minMagnitude);
+        uriBuilder.appendQueryParameter("orderby", "time");
+        uriBuilder.appendQueryParameter("orderby", orderBy);
+
+
+        return new NewsLoader(this, uriBuilder.toString());
+
     }
 
     @Override
@@ -146,5 +176,21 @@ public class NewsActivity extends AppCompatActivity
         Log.i(LOG_TAG, "TEST: onLoadReset() is called ...");
         // Loader reset, so we can clear out our existing data.
         mAdapter.clear();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            Intent settingsIntent = new Intent(this, SettingsActivity.class);
+            startActivity(settingsIntent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
